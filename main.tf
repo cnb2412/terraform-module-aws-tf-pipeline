a08009f8e0e1ae13c5819550d624aa96920d3fd7
Christian Niephaus <christian@cnb24.de>
2023-11-23 20:25:42 +0100

    tfstorage bucket

---
+++ main.tf
@@ -0,0 +1,13 @@
+# S3 Bucket for TF state file
+resource "aws_s3_bucket" "tf_state_bucket" {
+  bucket = "${var.resource_prefix}-tfstate-storage"
+  tags = {
+    aws_bootstrap = "true"
+  }
+}
+resource "aws_s3_bucket_versioning" "tf_state_bucket_versioning" {
+  bucket = aws_s3_bucket.tf_state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
